2f7d2b2709a9a5d4e5f18c1a365662cf178431f9
Adam Hendricksen <adam@nird.us>
2014-02-26 20:33:27 -0800

    Generating rails sans test unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codaccino::Application.config.secret_key_base = '10300d7a54320797874d9c8005b5c1c1734295a3881e108a284865e3be9db6d74327bfd7b9f3ac86ece04083b10de260eac6fd593b23c5b247b02532c0494a03'
