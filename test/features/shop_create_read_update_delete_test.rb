0036ebedd1bd3c9beab68d0c076506a6e4d9cb28
Adam Hendricksen <adam@nird.us>
2014-03-08 10:56:28 -0800

    Refactoring tests for style and using yml data

--- test/features/shop_create_read_update_delete_test.rb
+++ test/features/shop_create_read_update_delete_test.rb
@@ -1,19 +1,21 @@
-require "test_helper"
+require 'test_helper'
 
-feature "ShopCreateReadUpdateDelete" do
-  scenario "Adding a coffeeshop" do
+feature 'ShopCreateReadUpdateDelete' do
+  scenario 'Adding a coffeeshop' do
     visit new_shop_path
-    page.must_have_content "Where are you today?"
-    fill_in "Name", with: "Urban City Cafe"
-    fill_in "Address", with: "4313 212th St SW Mountlake Terrace, WA 98043"
-    fill_in "Site", with: "http://www.urbancitycoffee.com/content/indexretail.html"
-    fill_in "Phone", with: "(425) 776-1273"
-    fill_in "Wifi up", with: 20
-    fill_in "Wifi down", with: 11
-    fill_in "Outlet rating", with: 2
-    fill_in "Hrs wkday", with: "5:00 am - 10:00 pm"
-    fill_in "Hrs saturday", with: "6:00 am - 10:00 pm"
-    fill_in "Hrs sunday", with: "6:00 am - 8:00 pm"
-    click_on "Commit the Coffeeshop"
+    page.must_have_content 'Where are you today?'
+    fill_in 'Name', with: shops(:shop_1).name
+    fill_in 'Address', with: shops(:shop_1).address
+    fill_in 'Site', with: shops(:shop_1).site
+    fill_in 'Phone', with: shops(:shop_1).phone
+    fill_in 'Wifi up', with: 20
+    fill_in 'Wifi down', with: 11
+    fill_in 'Outlet rating', with: 2
+    fill_in 'Hrs wkday', with: shops(:shop_1).hrs_wkday
+    fill_in 'Hrs saturday', with: shops(:shop_1).hrs_saturday
+    fill_in 'Hrs sunday', with: shops(:shop_1).hrs_sunday
+    click_on 'Commit the Coffeeshop'
+    page.must_have_content 'data saved'
   end
+
 end
