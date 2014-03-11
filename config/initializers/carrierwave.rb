21976acf373dba3570603d76216fba4e8afd5669
Adam Hendricksen <adam@nird.us>
2014-03-11 16:41:52 -0700

    fix carrierwave:

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],     # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], # required
-    :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
-    :endpoint               => 'https://codaccino.s3-website-us-west-2.amazonaws.com' # optional, defaults to nil
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] # required
   }
   config.fog_directory  = 'codaccino'                        # required
-  config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
