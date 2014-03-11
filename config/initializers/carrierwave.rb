e2a690d85e9b2c3c0031342413bdef664342456b
Adam Hendricksen <adam@nird.us>
2014-03-11 15:53:54 -0700

    Adding missing comma

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],     # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], # required
     :region                 => 'Oregon',                     # optional, defaults to 'us-east-1'
     :host                   => 'codaccino.com',             # optional, defaults to nil
     :endpoint               => 'codaccino.s3-website-us-west-2.amazonaws.com' # optional, defaults to nil
